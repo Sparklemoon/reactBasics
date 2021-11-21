35a3390e99dfa0453888aa1944489c7047b6483f
Myra James <71600199+Sparklemoon@users.noreply.github.com>
2021-11-21 18:19:55 -0500

    added files and changed names

--- app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ReactDOM } from 'react-dom';
-
-
-ReactDOM.render(
-    React.createElement("ul",{style: {color:"blue"}}, "Hello"),
-    document.getElementById('root')
-);
-
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ReactDOM } from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+    React.createElement(
+        "ul",
+        {style: {color:"blue"} }, 
+        React.createElement("li", null, "Hot Dogs"),
+        React.createElement("li", null, "Hamburgers"),
+        React.createElement("li", null, "Pizza"),
+        React.createElement("li", null, "Sushi")
+        ),
+    document.getElementById('root')
+);
+
